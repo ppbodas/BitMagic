3f25d55b1c53d0c4ddef224ac0e4d9b7b06a68e0
Prathmesh Bodas <prathmesh@gmail.com>
2015-06-07 20:43:14 +0530

    Added code for toggling bits

--- SetBit/Source.cpp
+++ SetBit/Source.cpp
@@ -5,9 +5,15 @@
 
 using namespace std;
 
-int main()
+void printBits(int n)
 {
-	int i;
+	//print in binary format
+	bitset<sizeof(n) * 8> bits(n);
+	cout << bits << endl;
+}
+
+void printBitRepeatatively()
+{
 	string str;
 	cout <<"Enter any number or return key to stop: ";
 	while (std::getline(cin, str))
@@ -15,11 +21,53 @@ int main()
 		if (str.empty())
 			break;
 		istringstream strStream(str);
-		
+
+		int i;		
 		strStream >> i;
-		//print in binary format
-		bitset<sizeof(i) * 8> bits(i);
-		cout << bits << endl;
+		printBits(i);
 		cout <<"Enter any number or return key to stop: ";
 	}
+}
+
+void setbit(int n, int k)
+{
+	k = 0x1 << k;
+
+	cout<< "Original Bits: ";
+	printBits(n);
+
+	cout<< "Changed  Bits: ";
+	printBits(n | k);
+}
+
+void clearBit(int n, int k)
+{
+	k = 0x1 << k;
+
+	cout<< "Original Bits: ";
+	printBits(n);
+
+	cout<< "Changed  Bits: ";
+	printBits(n & ~k);
+}
+
+bool bitSetOrNot(int n, int k)
+{
+	k = 0x1 << k;
+
+	return (k & n);
+}
+
+void toggleBit(int n, int k)
+{
+	bitSetOrNot(n,k)? clearBit(n,k): setbit(n,k);
+}
+
+int main()
+{
+	//printBitRepeatatively();
+	//setbit(20,3);
+	//clearBit(20,2);
+	//bitSetOrNot(20, 2);
+	toggleBit(20,2);
 }
